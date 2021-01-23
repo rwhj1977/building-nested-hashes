2d88d34603f234774f6ad49d1eb51db7dd182d9d
Robert Hardy <rwhj77@hotmail.com>
2021-01-22 21:33:33 -0600

    commit tree f42d5763c97ad065ea2f543d51ad946c954d902a

--- lib/fifth_challenge.rb
+++ lib/fifth_challenge.rb
@@ -1,7 +1,5 @@
-require "pry"
 
 def fifth_challenge
-
   epic_tragedy = {
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
@@ -11,21 +9,23 @@ def fifth_challenge
          {name: "Benvolio", age: "17", attitude: "worried"},
          {name: "Steven", age: "30", attitude: "confused"}
       ]
-   }, 
+   },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-          {name: "Mercutio", age: "18", attitude: "hot-headed"}, 
+          {name: "Mercutio", age: "18", attitude: "hot-headed"},
           {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
   }
 
   #code your solution here:
+  epic_tragedy[:montague][:hero][:status] = "dead"
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
 
-
-  #Don't touch the following line! The method must return our newly modified epic tragedy hash
+  #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
 end
+
